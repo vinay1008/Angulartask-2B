cb9f75e6eca2335ddc4c9d62e67b498e81af41c6
vinay1008 <vinaypatil1718@gmail.com>
2023-07-12 14:37:36 +0530

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmailComponent } from './email/email.component';
+import { PhoneComponent } from './phone/phone.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"phone", component:PhoneComponent},
+  {path:"email", component:EmailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TwoComponent } from './two/two.component';
+import { EmailComponent } from './email/email.component';
+import { PhoneComponent } from './phone/phone.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TwoComponent,
+    EmailComponent,
+    PhoneComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
